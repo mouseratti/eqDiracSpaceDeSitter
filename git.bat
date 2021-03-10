b6390a8fdf468c1da0e2be9c29d5b95c1281d7ee
zanxi <consultantweb25@gmail.com>
2021-03-10 13:50:04 +0300

    345435435

---
+++ git.bat
@@ -0,0 +1,7 @@
+git remote add origin https://github.com/zanxi/eqDiracSpaceDeSitter.git
+git init
+git add README.md
+git commit -m "equation of Dirac in deSitter space"
+git branch -M main
+git remote add origin https://github.com/zanxi/eqDiracSpaceDeSitter.git
+git push -u origin main
